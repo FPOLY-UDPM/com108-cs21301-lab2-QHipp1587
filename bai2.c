503c22dd5d3504ca5d078bf8d4c0a77ae7507a2b
QHipp1587 <nguyenquochiep77@gmail.com>
2026-01-13 09:02:58 +0700

    hiep nop lap 2

--- bai2.c
+++ bai2.c
@@ -10,15 +10,29 @@
 
 #include <stdio.h>
 
-int main(){
-    // Khai báo biến
+#include <stdio.h>
 
-    // Nhập dữ liệu
+int main()
+{
+    // Khai báo 2 biến chiều dài và chiều rộng của hình chữ nhật
+    float ChieuDai, ChieuRong;
+    // Khai báo biến cho diện tích và chu vi của hình chữ nhật
+    float dienTich, chuVi;
 
+    // Người dùng nhập số đo chiều dài và chiều rộng
+    printf("Nhap chieu dai: ");
+    scanf("%f", &ChieuDai);
 
-    // Xử lý, tính toán
+    printf("Nhap chieu rong: ");
+    scanf("%f", &ChieuRong);
 
-    // Hiển thị kết quả
+    // Biểu thức tính toán của diện tích và chu vi hình chữ nhật
+    dienTich = ChieuDai * ChieuRong;
+    chuVi = (ChieuDai + ChieuRong) * 2;
 
+    // In ra màn hình kết quả diện tích và chu vi hình chữ nhật
+    printf("\nDien tich hinh chu nhat = %.2f\n", dienTich);
+    printf("Chu vi hinh chu nhat = %.2f\n", chuVi);
 
+    return 0;
 }
--- bai3.c
+++ bai3.c
@@ -10,15 +10,20 @@
 
 #include <stdio.h>
 
-int main(){
-    // Khai báo biến
+int main()
+{
+     float banKinh;
+    float chuVi, dienTich;
+    const float PI = 3.14159;
 
-    // Nhập dữ liệu
+    printf("Nhap vao ban kinh cua duong tron: ");
+    scanf("%f", &banKinh);
 
+    chuVi = 2 * PI * banKinh;
+    dienTich = PI * banKinh * banKinh;
 
-    // Xử lý, tính toán
+    printf("\nChu vi hinh tron = %.2f\n", chuVi);
+    printf("Dien tich hinh tron = %.2f\n", dienTich);
 
-    // Hiển thị kết quả
-
-
+    return 0;
 }
